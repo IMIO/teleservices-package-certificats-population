5a9681b9c8946332602ee9cddc10c75fa241f7a7
jmodesto <jordano.modesto@imio.be>
2024-03-06 15:23:21 +0100

    [TELE-1888]Add passerelle and install script

---
+++ teleservices_package_certificats_population/passerelle/certificats_population.py
@@ -0,0 +1,3 @@
+if 'passerelle_imio_apims_certificats_population' not in INSTALLED_APPS:
+    INSTALLED_APPS += ("passerelle_imio_apims_certificats_population",)
+    TENANT_APPS += ("passerelle_imio_apims_certificats_population",)
