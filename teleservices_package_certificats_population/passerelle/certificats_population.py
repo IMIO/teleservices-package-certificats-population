cb62f16f69c36638c4f8b76cd990904e99f6b85b
jmodesto <jordano.modesto@imio.be>
2024-03-21 13:56:31 +0100

    [TELE-1886]Test re-push passerelle

--- teleservices_package_certificats_population/passerelle/certificats_population.py
+++ teleservices_package_certificats_population/passerelle/certificats_population.py
@@ -1,3 +1,4 @@
+#test
 if 'passerelle_imio_apims_certificats_population' not in INSTALLED_APPS:
     INSTALLED_APPS += ("passerelle_imio_apims_certificats_population",)
     TENANT_APPS += ("passerelle_imio_apims_certificats_population",)
